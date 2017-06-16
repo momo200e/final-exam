2e9d19e2fa781256be55502958b62b7eaba3ecac
ss910316s910316ss <ss910316s910316ss@yahoo.com.tw>
2017-06-16 03:44:02 -0400

    finish

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,14 +1,14 @@
 module StudentsHelper 
   def a_grade(student)
-      if student.grade.to_i > 90
-        "S級(#{student.grade})"
-      elsif student.grade > 80 
+      if student.grade.to_i >= 90
+        "S級(#{student.grade})"
+      elsif student.grade >= 80 && student.grade < 90
         "A級(#{student.grade})"
      
-      elsif student.grade > 70 
+      elsif student.grade >= 70 && student.grade < 80
         "B級(#{student.grade})"
      
-      elsif student.grade > 60 
+      elsif student.grade >= 60 && student.grade < 70
         "C級(#{student.grade})"
 
       else
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,16 @@
 class Student < ActiveRecord::Base
+  def show_grade
+      if grade >= 90
+        "S級(#{grade})"
+      elsif grade > 80  && grade < 90
+        "A級(#{grade})"
+      elsif grade > 70  && grade < 80
+        "B級(#{grade})"
+      elsif grade > 60  && grade < 70
+        "C級(#{grade})"
+      else
+        "D級(#{grade})"
+      end
+      
+  end
 end
